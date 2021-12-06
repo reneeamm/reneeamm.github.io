31fde1aefe80d05fcd04eb8aa8a1178e3481e703
reneeamm <83306059+reneeamm@users.noreply.github.com>
2021-12-06 12:10:06 -0500

    Add files via upload

---
+++ Portfolio/contact.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Untitled Document</title>
+</head>
+<body>
+	
+Welcome <?php echo $_GET["name"]; ?><br>
+Your email address is <?php echo $_GET["email"]; ?> <br>
+Your phone number is <?php echo $_GET["number"]; ?> <br>
+Your comment is <?php echo $_GET["comment"]; ?>
+			 
+</body>
+</html>
+	
+	
+	
+	
+</body>
+</html>
